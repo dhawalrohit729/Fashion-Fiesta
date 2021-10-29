eb9e9f12636ba09a1e45716a3b12ae89870050bc
Dhawal rohit <43619839+dhawalrohit729@users.noreply.github.com>
2021-10-29 16:16:13 +0530

    Update ProductList.jsx

--- pages/ProductList.jsx
+++ pages/ProductList.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import Navbar from "../components/Navbar";
 import Announcement from "../components/Announcement";
-import Products from "../components/Products";
+import Products from "../components/Products"; 
 import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
 import { mobile } from "../responsive";
