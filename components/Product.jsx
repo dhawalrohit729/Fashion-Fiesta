4a48d31b56e754c55fe800ea7114d1760600b83a
Dhawal rohit <43619839+dhawalrohit729@users.noreply.github.com>
2021-10-29 16:18:05 +0530

    Update Product.jsx

--- components/Product.jsx
+++ components/Product.jsx
@@ -9,7 +9,7 @@ const Info = styled.div`
   opacity: 0;
   width: 100%;
   height: 100%;
-  position: absolute;
+  position: absolute; 
   top: 0;
   left: 0;
   background-color: rgba(0, 0, 0, 0.2);
